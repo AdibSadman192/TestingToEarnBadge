9b58433aea55f6aca1c68f2e13e133dbcd62eb00
Adib Sadman <61665091+AdibSadman192@users.noreply.github.com>
2025-06-06 01:36:20 +0600

    updare

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const markCommit = (x, y) => {
 
 const makeCommits = (n) => {
   if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
+  const x = random.int(0, 74);
+  const y = random.int(0, 9);
   const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
 
   const data = {
